002c37e0e858015f5806837dfc2649f7a968f42d
Santosh Kumar MOrya <santoshmorya400@gmail.com>
2022-04-06 15:19:13 +0530

    Java Script demo

---
+++ web development/JavaScript/first.js
@@ -0,0 +1,81 @@
+
+/*
+there are 6 primitive datatype in javascript :
+number : 64bit
+String
+boolean
+null
+undefined
+symbol
+
+One non-primitive:
+1. Object
+*/
+
+
+
+/*
+Null vs Undefined :-
+
+*/
+
+
+
+
+
+/*
+Math.pow(10,200);
+
+Math.pow(10,100000000000000000000000000000);
+Infinity
+
+to get the max value can be stored in javaScript  = Number.MAX_VALUE;
+1.7976931348623157e+308
+
+
+*/
+
+
+/*
+NaN - Not a number
+
+EX: 
+    Math.sqrt(-1);
+    Nan
+
+
+    parseInt("aaaa");
+    Nan
+
+
+*/
+
+/*
+typeof 10;
+"answer"
+
+var a = "fdf";
+typeof a;
+string
+
+typeof null;
+object     (It is a bug in java script)
+
+typeof Infinity
+"number"
+
+typeof Nan;
+"number"
+*/
+
+
+let hello  = document.querySelector('button');
+hello.addEventListener('click', input);
+// function show() {
+//     alert("Hello world");
+// }
+
+function input(){
+let name = prompt('Enter Name of student');
+hello.textContent = 'Roll No. 1:' + name;
+}
