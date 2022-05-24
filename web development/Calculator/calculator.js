5fcb9879d41919c8b3cf9511a475ece4f7b89afe
Santosh Kumar MOrya <santoshmorya400@gmail.com>
2022-05-25 02:50:47 +0530

    Calculator java script

--- web development/Calculator/calculator.js
+++ web development/Calculator/calculator.js
@@ -1,46 +1,73 @@
-var a = '';
-var b = '';
-var num = [];
-var ans;
+// var a = '';
+// var b = '';
+// var num = [];
+// var ans;
 
-function sendNum(digit){
-    num.push(digit);
+// function sendNum(digit){
+//     num.push(digit);
 
-    if(num.length != 1){
-        a = '';
-        document.getElementById('screen').innerHTML = a;
+//     if(num.length != 1){
+//         a = '';
+//         document.getElementById('screen').innerHTML = a;
 
-    }
+//     }
 
-    for(i = 0;i<num.length;i++){
-        a += num[i];
-    }
+//     for(i = 0;i<num.length;i++){
+//         a += num[i];
+//     }
 
-    document.getElementById('screen').innerHTML = a;
-}
+//     document.getElementById('screen').innerHTML = a;
+// }
 
-function equalTo(){
-    document.getElementById('screen').innerHTML = '';
-    for(i = 0;i<num.length;i++){
-        b += num[i];
-    }
-    ans = eval(b);
+// function equalTo(){
+//     document.getElementById('screen').innerHTML = '';
+//     for(i = 0;i<num.length;i++){
+//         b += num[i];
+//     }
+//     ans = eval(b);
 
-    document.getElementById('screen').innerHTML = ans;
+//     document.getElementById('screen').innerHTML = ans;
 
-    while(num.length > 0){
-        num.pop();
-    }
+//     while(num.length > 0){
+//         num.pop();
+//     }
 
-    num.push(ans.toString());
-}
+//     num.push(ans.toString());
+// }
 
-function clearScr(){
-    document.getElementById('screen').innerHTML= '';
-    while(num.length >0){
-        num.pop;
-    }
-    a = '';
-    b = ''; 
+// function clearScr(){
+//     document.getElementById('screen').innerHTML= '';
+//     while(num.length >0){
+//         num.pop;
+//     }
+//     a = '';
+//     b = ''; 
+
+// }
+
+
+let screen = document.getElementById('screen');
+buttons = document.querySelectorAll('button');
+let screenValue = '';
+for(item of buttons){
+    item.addEventListener('click',(e)=>{
+        buttonText = e.target.innerText;
+        console.log('button text is ',buttonText);
+
+        if(buttonText == 'X'){
+            buttonText = '*';
+            screenValue  += buttonText;
+            screen.value  = screenValue;
+        }else if(buttonText == 'C'){
+            screenValue = "";
+            screen.value = screenValue;
+        }else if (buttonText == '='){
+                screen.value = eval(screenValue);
+        }else{
+            screenValue += buttonText;
+            screen.value = screenValue;
+        }
+    })
+
 
 }
