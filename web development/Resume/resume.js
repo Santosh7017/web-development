c5aa0fe63d32d7bbafe3aedec89c28dfd36c4830
Santosh Kumar MOrya <santoshmorya400@gmail.com>
2022-06-23 01:09:29 +0530

    resume

--- web development/Resume/resume.js
+++ web development/Resume/resume.js
@@ -1,10 +1,32 @@
-// var scrollInterval =  setInterval(function(){
-//     window.scrollBy(0,50);
-// },50)
+var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
+// console.log(navMenuAnchorTags);  // to get the numbe of anchor tags
+var interval
 
+for(var i = 0; i<navMenuAnchorTags.length; i++){
+    navMenuAnchorTags[i].addEventListener('click',function(event){
+       event.preventDefault();
 
+       var targetSectionID = this.textContent.trim().toLowerCase();
+       var targetSection = document.getElementById(targetSectionID);
 
+       console.log(targetSection);
+       
 
-//     clearInterval(scrollInterval); {
-//     }
-// }
+    
+    //  interval = setInterval(scrollVertically,30,targetSection);
+
+    interval = setInterval(function(){
+        scrollVertically(targetSection);
+    },30,targetSection);
+
+});
+}
+
+function scrollVertically(targetSection){
+    var targetSectionCoordinates = targetSection.getBoundingClientRect();
+        if(targetSectionCoordinates.top <= 0){
+            clearInterval (interval);
+            return;
+        }
+            window.scrollBy(0,50)
+}
