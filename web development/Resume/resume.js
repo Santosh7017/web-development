c5706842d4c2da2086bf519f44b5eca55027680f
Santosh Kumar MOrya <santoshmorya400@gmail.com>
2022-06-25 03:12:22 +0530

    resume javaScript

--- web development/Resume/resume.js
+++ web development/Resume/resume.js
@@ -1,32 +1,83 @@
-var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
-// console.log(navMenuAnchorTags);  // to get the numbe of anchor tags
-var interval
+// var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
+// // console.log(navMenuAnchorTags);  // to get the numbe of anchor tags
+// var interval
 
-for(var i = 0; i<navMenuAnchorTags.length; i++){
-    navMenuAnchorTags[i].addEventListener('click',function(event){
-       event.preventDefault();
+// for(var i = 0; i<=navMenuAnchorTags.length; i++){
+//     navMenuAnchorTags[i].addEventListener('click',function(event){
+//        event.preventDefault();
 
-       var targetSectionID = this.textContent.trim().toLowerCase();
-       var targetSection = document.getElementById(targetSectionID);
+//        var targetSectionID = this.textContent.trim().toLowerCase();
+//        var targetSection = document.getElementById(targetSectionID);
 
-       console.log(targetSection);
+//     //    console.log(targetSection);
        
 
     
-    //  interval = setInterval(scrollVertically,30,targetSection);
+//     //  interval = setInterval(scrollVertically,30,targetSection);
 
-    interval = setInterval(function(){
-        scrollVertically(targetSection);
-    },30,targetSection);
+//     interval = setInterval(function(){
+//         scrollVertically(targetSection);
+//     },30,targetSection);
 
-});
+// });
+// }
+
+// function scrollVertically(targetSection){
+//     var targetSectionCoordinates = targetSection.getBoundingClientRect();
+//         if(targetSectionCoordinates.top <= 0){
+//             clearInterval (interval);
+//             return;
+//         }
+//             window.scrollBy(0,50)
+// }
+
+
+var progressBars = document.querySelectorAll('.skill-progress > div');
+var skillsContainer = document.getElementById('skills-container');
+window.addEventListener('scroll', checkScroll);
+var animationDone = false;
+
+
+function initialiseBars(){
+    for(let bar of progressBars){
+        bar.style.width = 0 + '%';
+    }
 }
+initialiseBars();
 
-function scrollVertically(targetSection){
-    var targetSectionCoordinates = targetSection.getBoundingClientRect();
-        if(targetSectionCoordinates.top <= 0){
-            clearInterval (interval);
+function fillBars(){
+    for(let bar of progressBars){
+    let targetWidth = bar.getAttribute('data-bar-width');
+    let currentWidth = 0;
+    let interval = setInterval(function()  {
+        if (currentWidth > targetWidth) {
+            clearInterval(interval);
             return;
         }
-            window.scrollBy(0,50)
+        currentWidth++;
+        bar.style.width = currentWidth + '%';
+    },5);
+
+    }
+
+}
+
+
+
+function checkScroll(){
+// check wheather skill container is visible
+
+var coordinates = skillsContainer.getBoundingClientRect();
+if(!animationDone && coordinates.top < window.innerHeight ){
+    animationDone = true;
+    console.log('Skill Section Visible');
+    fillBars();
+
+}
+// else if(coordinates.top < window.innerHeight){
+//     animationDone = false;
+//     initialiseBars();
+
+// }
+
 }
