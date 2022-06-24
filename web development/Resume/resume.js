11036932f5436ef04ecb7f2f7cf612c844a257af
Santosh Kumar MOrya <santoshmorya400@gmail.com>
2022-06-25 04:05:17 +0530

    resume js

--- web development/Resume/resume.js
+++ web development/Resume/resume.js
@@ -1,36 +1,39 @@
-// var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
-// // console.log(navMenuAnchorTags);  // to get the numbe of anchor tags
-// var interval
+var navMenuAnchorTags = document.querySelectorAll('.nav-menu a');
+// console.log(navMenuAnchorTags);  // to get the numbe of anchor tags
+var interval
 
-// for(var i = 0; i<=navMenuAnchorTags.length; i++){
-//     navMenuAnchorTags[i].addEventListener('click',function(event){
-//        event.preventDefault();
+for(var i = 0; i<=navMenuAnchorTags.length; i++){
+    navMenuAnchorTags[i].addEventListener('click',function(event){
+       event.preventDefault();
 
-//        var targetSectionID = this.textContent.trim().toLowerCase();
-//        var targetSection = document.getElementById(targetSectionID);
+       var targetSectionID = this.textContent.trim().toLowerCase();
+       var targetSection = document.getElementById(targetSectionID);
 
-//     //    console.log(targetSection);
+    //    console.log(targetSection);
        
 
     
-//     //  interval = setInterval(scrollVertically,30,targetSection);
+    //  interval = setInterval(scrollVertically,30,targetSection);
 
-//     interval = setInterval(function(){
-//         scrollVertically(targetSection);
-//     },30,targetSection);
+    interval = setInterval(function(){
+        scrollVertically(targetSection);
+    },30,targetSection);
 
-// });
-// }
+});
+}
 
-// function scrollVertically(targetSection){
-//     var targetSectionCoordinates = targetSection.getBoundingClientRect();
-//         if(targetSectionCoordinates.top <= 0){
-//             clearInterval (interval);
-//             return;
-//         }
-//             window.scrollBy(0,50)
-// }
+function scrollVertically(targetSection){
+    var targetSectionCoordinates = targetSection.getBoundingClientRect();
+        if(targetSectionCoordinates.top <= 0){
+            clearInterval (interval);
+            return;
+        }
+            window.scrollBy(0,50)
+}
 
+
+
+// animated scroll bar
 
 var progressBars = document.querySelectorAll('.skill-progress > div');
 var skillsContainer = document.getElementById('skills-container');
