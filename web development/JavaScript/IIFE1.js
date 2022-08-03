3fed8f2769b4cabaa9cab435f4768a36c0336e45
Santosh Kumar Morya <santoshmorya400@gmail.com>
2022-08-03 22:24:50 +0530

    IIFE

---
+++ web development/JavaScript/IIFE1.js
@@ -0,0 +1,9 @@
+// (   function()  {
+//     var name = "File1"
+//     setTimeout( function(){console.log(name)},1000);
+   
+// })();
+{
+let fileName = "File1"
+    setTimeout( function(){console.log(fileName)},1000);
+}
---
+++ web development/JavaScript/IIFE2.js
@@ -0,0 +1,10 @@
+// (
+//     function(){
+//         var name = "File2"
+//         console.log(name);
+//     })
+// ();
+{
+            var fileName = "File2"
+            console.log(fileName);
+        };
