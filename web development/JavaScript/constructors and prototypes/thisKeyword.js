d208345ec42705e11e2712e3c9c4f8ef6d350f47
Santosh Kumar Morya <santoshmorya400@gmail.com>
2022-08-18 22:59:20 +0530

    this keyword

--- web development/JavaScript/constructors and prototypes/thisKeyword.js
+++ web development/JavaScript/constructors and prototypes/thisKeyword.js
@@ -1,21 +1,25 @@
 
-
-function demo(){
+'use strict';
+function demo(a,b){
     console.log(this);
+    console.log(a,b);
 }
 
-var demo2 = function (){
-    console.log(this);
-}
+// var demo2 = function (){
+//     console.log(this);
+// }
 
 var obj = {
     'prop1': 12,
-    'prop2' : 90,
+    // 'prop2' : 90,
     'print' : function(){
         console.log(this);
     }
 }
-obj.print();
+// demo.call(obj); // bound the object and because of strict  it will not pick any default value
+// demo.call(obj, 3,4);
+demo.apply(obj, [3,4]); // difference between call and argument is in case of arguments
+// obj.print();
 
 // console.log(this);
 // demo();
