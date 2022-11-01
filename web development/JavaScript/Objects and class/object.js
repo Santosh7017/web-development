af53a6e2831dbc621ac61acdfed7a2f494816a11
Santosh Kumar Morya <santoshmorya400@gmail.com>
2022-11-02 02:30:39 +0530

    object in js

---
+++ web development/JavaScript/Objects and class/object.js
@@ -0,0 +1,12 @@
+// two types to declare objects
+
+// var obj = {};
+// var obj = new Object();
+
+// var obj = new Object();
+// console.log(Object.getPrototypeOf(obj));
+// console.log(Object.prototype.isPrototypeOf(obj));
+// Object.prototype.color = "black";
+// var obj = {};
+// console.log(obj.color);
+
