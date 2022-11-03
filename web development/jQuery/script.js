613f63115f859e758ab83c3512e148e252102fc9
Santosh Kumar Morya <santoshmorya400@gmail.com>
2022-11-04 00:11:21 +0530

    modifying css using query

---
+++ web development/jQuery/script.js
@@ -0,0 +1,17 @@
+// var para1 = document.getElementById('para1');
+// para1.innerHTML = "Welcome";
+
+//  $('#para1').html('Welcome to web de');
+/*
+    function $(query){
+        document.querySelector(query);
+    }
+*/
+// $('#para1').hide();
+// $('#para1').width(10);
+// $('p').html("wecome");
+$("p").css({
+    fontSize:"30px",
+    color: "red"
+
+});
