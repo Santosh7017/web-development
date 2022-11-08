f8aac4080cf61adcc890bae57ed0e3851de4b619
Santosh Kumar Morya <santoshmorya400@gmail.com>
2022-11-08 11:20:10 +0530

    node js intro

---
+++ web development/Node JS/index.js
@@ -0,0 +1,9 @@
+// // console.log("Hello world");
+// function add(a,b){
+//     return a+b;
+// }
+// console.log(add(2,8));
+// console.log(process.argv);
+// var args = process.argv.slice(2);
+// console.log("adding the numebrs: ", add(parseInt(args[0]),parseInt(args[1])));
+
