60b3b8f4a5307709493d8072c489d414ed21403e
Santosh Kumar Morya <santoshmorya400@gmail.com>
2022-11-09 09:21:36 +0530

    running server

--- web development/Node JS/basicHTTPserver/index.js
+++ web development/Node JS/basicHTTPserver/index.js
@@ -1,12 +1,28 @@
 const http = require('http');
 const port = 8000;
+const fs = require('fs');
+// const { url } = require('inspector');
 
+
+// function requestHandler(req, res){
+    // console.log(req.url);
+    // res.writeHead(200,{'content-type':'Text/html'});
+    // fs.readFile('./index.html');
+    // res.end('<h1>Gotcha!</h1>');
+    // res.end("Ended");
+// }
 
 function requestHandler(req, res){
-    console.log(req.url);
-    res.writeHead(200,{'content-type':'Text/html'})
-    res.end('<h1>Gotch!</h1>');
-    res.end("Ended");
+    console.log(req, url);
+    res.writeHead(200,{'content-type':'text/html'});
+    fs.readFile('./index.html',function(err,data){
+if(err){
+    console.log('err ',err);
+    return res.end('<h1>Error!</h1>');
+}
+return res.end(data);
+
+    })
 }
 
 
@@ -18,5 +34,5 @@ if(err){
     console.log(err);
     return;
 }
-console.log("server is up and running on port: ",port);
+console.log("server is running in local server: ",port);
 });
