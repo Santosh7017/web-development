1d69435128a9f28741e38ea2e8a0435c7035b27c
Santosh Kumar Morya <santoshmorya400@gmail.com>
2022-12-15 10:49:24 +0530

    normal updates5

--- Frontend/Ajax/intro.js
+++ Frontend/Ajax/intro.js
@@ -1,1 +1,25 @@
-//  Ajax means asynchronus java script and XML
+// Ajax means asynchronus java script and XML
+// xms is tricky json is simple and widly accepted 
+// http - hypertext transfer protocol
+// protocols - decides set of rules for communication
+// * client req cycle :-
+// 1) client opens up connection with server
+// 2) client makes a req to server
+// 3) server will processs the req
+// 4) server will send response to the client
+// 5) client will close the connection
+
+// * get and post
+// get to fetch data
+// store or send some result to store
+// get is passed in query paramenter whereas in post request passed in message body and hidden
+
+
+// if responses code is 200 it means succecfull
+//  401 client is not authorized
+// http is stateless - no information around the req maintained by server
+
+// * JSON - Javascript object notation
+
+// * API's
+//  
