cc7799c34a2ec105f6e2270d47deac06b1121235
Santosh Kumar Morya <santoshmorya400@gmail.com>
2022-12-27 02:59:05 +0530

    small changes

--- Backend/Node JS/index.js
+++ Backend/Node JS/index.js
@@ -1,9 +1,9 @@
 // // console.log("Hello world");
-// function add(a,b){
-//     return a+b;
-// }
-// console.log(add(2,8));
-// console.log(process.argv);
-// var args = process.argv.slice(2);
-// console.log("adding the numebrs: ", add(parseInt(args[0]),parseInt(args[1])));
+function add(a,b){
+    return a+b;
+}
+console.log(add(2,8));
+console.log(process.argv);
+var args = process.argv.slice(2);
+console.log("adding the numebrs: ", add(parseInt(args[0]),parseInt(args[1])));
 
