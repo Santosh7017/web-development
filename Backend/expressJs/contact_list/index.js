2ea66569b849b5f26dccd9a514d6b390a21ff4ca
Santosh Kumar Morya <santoshmorya400@gmail.com>
2022-12-28 23:48:33 +0530

    basic contact list using express js

--- Backend/expressJs/contact_list/index.js
+++ Backend/expressJs/contact_list/index.js
@@ -14,11 +14,29 @@ const port = 8000;
 const app = express();
 app.set('view engine','ejs');
 app.set('views',path.join(__dirname,'views'));
+app.use(express.urlencoded()); // parser, midelware  - a function whcih has acces to your request and response then it can process data and response data and have capability to return data.  
+app.use(express.static('assets'));
+
+//  creating middleware1
+// app.use(function(req, res, next){
+// // console.log("middleware 1 called");
+// req.myName="Arpan";
+// next();
+// });
+
+// //  middleware 2
+// app.use(function(req,res,next){
+//     console.log("My name from Mw2", req.myName);
+//     // console.log("middleware 2 called");
+//     next();
+// });
+
+
 
 var contactList = [
 {
     name:"Arpan",
-    Phone:"1111111111",
+    Phone:"3938274638",
 },
 {
     name:"Tony  Stark",
@@ -38,6 +56,7 @@ var contactList = [
 
 
 app.get('/',function(req,res){
+    console.log("From the get route controller",req.myName); 
     return res.render('home',{
         title: "Contact List",
         contact_List: contactList
@@ -57,6 +76,23 @@ title:"Let us play with ejs"
     });
 });
 
+app.post("/create-contact",function(req, res){
+    // contactList.push({
+    //     name:req.body.name,
+    //     Phone:req.body.Phone
+
+    // });
+
+    contactList.push(req.body);
+    return res.redirect('back');
+
+    // return res.redirect('/');
+
+    // console.log(req.body);
+    // console.log(req.body.name);
+    // console.log(req.body.Phone);
+    // return res.redirect('/practice');
+});
 
 
 app.listen(port, function(err){
@@ -64,4 +100,5 @@ app.listen(port, function(err){
         console.log("Error in running the server");
     }
     console.log("Express server is running on port: ",port);
+
 });
